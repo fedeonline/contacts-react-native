916e091fca3bd12a873fd86b0e8518c04bb67d2a
Federico Pugnaloni <federico.pugnaloni@streetlib.com>
2019-02-20 13:58:18 +0100

    First commit. Use navigation parameter to pass props to next screen.

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import AppNavigator from './app/routes';
+
+export default function App() {
+  return <AppNavigator />;
+}
---
+++ app/routes.js
@@ -0,0 +1,18 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+import Contacts from './screens/Contacts';
+import Profile from './screens/Profile';
+import colors from './utils/colors';
+
+const AppNavigator = createStackNavigator({
+    Contacts: {
+        screen: Contacts,
+    },
+    Profile: {
+        screen: Profile,
+    },
+}, { 
+    initialRouteName: 'Contacts',
+});
+
+export default createAppContainer(AppNavigator);
